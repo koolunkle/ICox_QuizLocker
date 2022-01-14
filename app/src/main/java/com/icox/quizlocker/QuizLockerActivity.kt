f17e91c198be1661b33d37e06e0816f2bca22fac
koolunkle <gugi329@daum.net>
2022-01-14 17:29:17 +0900

    잠금 화면용 Activity UI 구현

---
+++ app/src/main/java/com/icox/quizlocker/QuizLockerActivity.kt
@@ -0,0 +1,11 @@
+package com.icox.quizlocker
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class QuizLockerActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_quiz_locker)
+    }
+}
